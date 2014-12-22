8d4175f2e97015a4b2fb5e7a855b7efff6e32c0c
it0a <recursive404@gmail.com>
2014-12-22 00:51:37 -0500

    Making names vary by some hash code

--- app/paste/messages.directive.js
+++ app/paste/messages.directive.js
@@ -19,6 +19,9 @@
                     });
                     scope.message = '';
                 };
+                scope.userClass = function (user) {
+                  return "username" + (Math.abs(user.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a;},0) % 10) + 1);
+                };
             },
             templateUrl: 'paste/views/messages.html'
         };
